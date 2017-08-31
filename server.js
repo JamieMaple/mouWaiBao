e405ef20ed8a97b4acf30922fa58d06fc6ffc7cf
JamieMaple <jamiemaple007@gmail.com>
2017-08-31 08:46:30 +0800

    add some files

--- server.js
+++ server.js
@@ -3,17 +3,30 @@ const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
 
-app.use('/portal/phone/iphone', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+app.get('/phone', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'phone.html'))
+})
 
 app.use(bodyParser.json({limit: '1mb'}));  //body-parser 解析json格式数据
 app.use(bodyParser.urlencoded({            //此项必须在 bodyParser.json 下面,为参数编码
   extended: true
 }));
 
-var appData = require('./data.json')
 // post
+app.post('/portal/phone/brand', (req, res) => {
+  var brand = require('./brand.json')
+  res.send(brand)
+})
 app.post('/portal/phone/modex', (req, res) => {
-  res.send(appData)
+  var appData = require('./data.json')
+  if(req){
+    res.send(appData)
+  }
 })
 app.post('/portal/phone/price', (req, res) => {
   console.log(req.body)
